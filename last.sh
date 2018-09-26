201618670decdbd72039d9832206b5bc5c8d3671
aadebuger <zhvxxh@gmail.com>
2018-09-26 16:25:09 +0800

    first commit

---
+++ last.sh
@@ -0,0 +1,3 @@
+ ~/ethereumvenv36/bin/python export_blocks_and_transactions.py --start-block 6300\
+001 --end-block 6400000 --provider-uri file:/home/aadebuger/GEXTMAC/ethereum1t/data/geth.ipc --blocks-output /home/aadebuger/GEXTMAC/home/aadebuger/github2018/ethereum-etl/data/blocks6300001.csv --transactions-output /home/aadebuger/GEXTMAC/home/aadebuger/github2018/ethereum-etl/data/transactions6300001.csv
+
